7a0c2423f1f1f2293ced26c33894c6726789ed9f
Abhinandan Rai <90084673+abhinanndan3492@users.noreply.github.com>
2024-12-09 12:30:38 +0530

    used placeholders

--- SQL-Node/index.js
+++ SQL-Node/index.js
@@ -1,4 +1,30 @@
 const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "delta",
+  password: "",
+});
+
+let q = "INSERT INTO user (id,username,email,password) VALUES ? ";
+let users = [
+  ["1234", "123_newuser", "abcd@gmail.com", "abcd1234"],
+  ["12345", "1234_newuser", "abcde@gmail.com", "abbcd1234"],
+];
+
+try {
+  connection.query(q, [users], (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    console.log(result.length);
+  });
+} catch (error) {
+  console.log(err);
+}
+
+connection.end();
 
 let getRandomUser = () => {
   return {
@@ -9,4 +35,4 @@ let getRandomUser = () => {
   };
 };
 
-console.log(getRandomUser());
+// console.log(getRandomUser());
