78acf41370ba051360dd2e28cb85ec7919744620
Abhinandan Rai <90084673+abhinanndan3492@users.noreply.github.com>
2024-12-09 12:50:27 +0530

    done with faker.js

--- SQL-Node/index.js
+++ SQL-Node/index.js
@@ -9,13 +9,29 @@ const connection = mysql.createConnection({
 });
 
 let q = "INSERT INTO user (id,username,email,password) VALUES ? ";
-let users = [
-  ["1234", "123_newuser", "abcd@gmail.com", "abcd1234"],
-  ["12345", "1234_newuser", "abcde@gmail.com", "abbcd1234"],
-];
+// let users = [
+//   ["1234", "123_newuser", "abcd@gmail.com", "abcd1234"],
+//   ["12345", "1234_newuser", "abcde@gmail.com", "abbcd1234"],
+// ];
+
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.username(), // before version 9.1.0, use userName()
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+
+let data = [];
+for (let index = 1; index <= 100; index++) {
+  data.push(getRandomUser());
+}
+
+// console.log(getRandomUser());
 
 try {
-  connection.query(q, [users], (err, result) => {
+  connection.query(q, [data], (err, result) => {
     if (err) throw err;
     console.log(result);
     console.log(result.length);
@@ -25,14 +41,3 @@ try {
 }
 
 connection.end();
-
-let getRandomUser = () => {
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
-};
-
-// console.log(getRandomUser());
